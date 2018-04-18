9c2e54af8d11a5541c4c7e9caca21dff1c65635a
Hector Menendez <hector@gik.mx>
2018-04-18 13:36:58 -0500

    First version adapted from Gomodoro project. Untested.

---
+++ README.md
@@ -0,0 +1,13 @@
+# [@gik/redux-factory](https://github.com/gikmx/redux-factory#readme) *0.0.4*
+> A shorthand for our redux/redux-thunk workflow
+
+##### Contributors
+- [Héctor Menéndez](mailto:hector@gik.mx) []()
+
+##### Supported platforms
+- darwin
+- linux
+
+#### <a name="table-of-contents"></a> Table of contents
+
+
---
+++ src/index.js
@@ -0,0 +1,75 @@
+import PropTypes from 'prop-types';
+import Thunk from 'redux-thunk';
+import { composeWithDevTools as ComposeWithDevTools } from 'redux-devtools-extension';
+import {
+    createStore as CreateStore,
+    combineReducers as CombineReducers,
+    applyMiddleware as ApplyMiddleware,
+} from 'redux';
+
+export { Provider } from 'react-redux';
+
+/**
+ * Store creator.
+ * Shorthand for Redux's CreateStore.
+ *
+ * @param {Object} reducers - An object containing the reducers to register.
+ * @param {Object} initStore - The initial value of the store.
+ * @param {Array}  middleware - An array of middlewares to register.
+ * @returns {ReduxStore} - The store to pass to the provider.
+ */
+export function Store(reducers, initStore, middleware = []) {
+
+    const reduxmiddleware = ApplyMiddleware(...middleware.concat(Thunk));
+
+    return CreateStore(
+        CombineReducers(reducers), // Root reducer
+        initStore,
+        ComposeWithDevTools({})(reduxmiddleware),
+    );
+}
+
+/**
+ * Actions and Reducers factory.
+ * Shorthand for reducing boilerplate needed to cerate reducers and actions.
+ *
+ * @param {Object} initState - The initial state for the reducer.
+ * @param {Object} declarations - An object contaning an action and a reducer.
+ * @param {string} prefix - An optional prefix for the actions/reducers.
+ * @returns {Object} - A { Actions, Reducers } object.
+ */
+export function Factory(initState, declarations, prefix) {
+
+    PropTypes.checkPropTypes(
+        {
+            initState: PropTypes.any.isRequired,
+            declarations: PropTypes.objectOf(PropTypes.shape({
+                action: PropTypes.func.isRequired,
+                reducer: PropTypes.func.isRequired,
+            })),
+            prefix: PropTypes.string,
+        },
+        { initState, declarations, prefix },
+        'arguments',
+        'FactoryRedux',
+    );
+    const keys = Object.keys(declarations);
+    const prefixer = value => prefix ? `${prefix}/${value}` : value;
+    return {
+
+        Actions: keys.reduce((acc, key) => {
+            const type = prefixer(key);
+            const fabricatedAction = declarations[key].action.bind(null, type);
+            return { ...acc, [key]: fabricatedAction };
+        }, {}),
+
+        Reducers: function fabricatedReducers(prevState = initState, { type, payload }) {
+            const match = keys
+                .map(key => ({ key: prefixer(key), reducer: declarations[key].reducer }))
+                .filter(({ key }) => key === type);
+            // No matching declaration found, this is most likely @@INIT, return state.
+            if (!match.length) return prevState;
+            return match[0].reducer(prevState, payload);
+        },
+    };
+}
